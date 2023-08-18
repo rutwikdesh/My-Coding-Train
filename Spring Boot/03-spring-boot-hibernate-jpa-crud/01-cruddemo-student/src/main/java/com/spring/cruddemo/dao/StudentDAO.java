d21db295355b65a3b859e2f67341254a2e666eaf
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2023-08-18 13:16:08 +0530

    CRUD App: Find Student by LastName

--- Spring Boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/spring/cruddemo/dao/StudentDAO.java
+++ Spring Boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/spring/cruddemo/dao/StudentDAO.java
@@ -2,7 +2,13 @@ package com.spring.cruddemo.dao;
 
 import com.spring.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
      void save(Student theStudent);
      Student findById(Integer id);
+
+     List<Student> findAll();
+
+     List<Student> findByLastName(String theLastName);
 }
--- Spring Boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/spring/cruddemo/dao/StudentDAOImpl.java
+++ Spring Boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/spring/cruddemo/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.spring.cruddemo.dao;
 
 import com.spring.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO{
@@ -28,5 +31,31 @@ public class StudentDAOImpl implements StudentDAO{
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        // Create Query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student ORDER BY lastName", Student.class);
+
+        // Return Query results
+        return theQuery.getResultList();
+
+        // Example Queries
+        // "FROM Student"
+        // "FROM Student ORDER BY lastName"
+        // "FROM Student ORDER BY lastName DESC"
+    }
+
+    @Override
+    public List<Student> findByLastName(String theLastName) {
+        // Create Query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student WHERE lastName=:theData", Student.class);
+
+        // Set Query Parameters
+        theQuery.setParameter("theData", theLastName);
+
+        // Return Query Results
+        return theQuery.getResultList();
     }
 }

895641a1010c3b7ac04e338f4f343f80de17af17
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2023-08-19 21:44:38 +0530

    CRUD Employees: Add Employees

--- Spring Boot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/springdemo/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ Spring Boot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/springdemo/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -25,4 +25,22 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO{
         List<Employee> employees = theQuery.getResultList();
         return employees;
     }
+
+    @Override
+    public Employee findById(int theId) {
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+        Employee dbEmployee = entityManager.merge(theEmployee);
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        entityManager.remove(theEmployee);
+    }
 }
--- Spring Boot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/springdemo/cruddemo/rest/EmployeeRestController.java
+++ Spring Boot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/springdemo/cruddemo/rest/EmployeeRestController.java
@@ -4,9 +4,7 @@ import com.springdemo.cruddemo.dao.EmployeeDAO;
 import com.springdemo.cruddemo.entity.Employee;
 import com.springdemo.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,25 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAllEmployees(){
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee findEmployee(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+        if(theEmployee == null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        return theEmployee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+
+        // In case they pass an id in json, set id to 0
+        // so that to force a save of the item ... instead of update
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
     }
 }

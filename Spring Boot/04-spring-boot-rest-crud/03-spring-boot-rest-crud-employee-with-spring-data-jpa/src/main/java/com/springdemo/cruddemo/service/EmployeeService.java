1c3c1cd4089919e152826ca0c4e4c9517344b20f
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2023-08-22 11:05:26 +0530

    Spring REST Security start

---
+++ Spring Boot/04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/springdemo/cruddemo/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.springdemo.cruddemo.service;
+
+import com.springdemo.cruddemo.entity.Employee;
+
+import java.util.List;
+
+
+public interface EmployeeService {
+    List<Employee> findAll();
+    Employee findById(int theId);
+    Employee save(Employee theEmployee);
+    void deleteById(int theId);
+}

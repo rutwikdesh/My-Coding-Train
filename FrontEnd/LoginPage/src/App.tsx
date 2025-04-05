a611b4fb0cc95ce94b3fa1cb4bf45adea8640a22
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2025-04-05 20:42:44 +0530

    added login page project

---
+++ FrontEnd/LoginPage/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <>
+      <Login />
+      {/* <div className="text-5xl border-1 p-10 m-20">Hi there!</div> */}
+    </>
+  );
+}
+
+export default App;
---
+++ Problem Solving/Graphs/main.py
@@ -0,0 +1,4 @@
+d = {'c': 0}
+if 'c' in d:
+  d['c'] += 1
+print(d)
